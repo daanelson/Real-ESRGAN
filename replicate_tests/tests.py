223a9c022228592a1c80e381f759684c0b2ee8c2
Dan Nelson <dan.nelson8@gmail.com>
2024-02-27 22:26:19 +0000

    model framework

--- replicate_tests/tests.py
+++ replicate_tests/tests.py
@@ -1,28 +1,23 @@
 """
 It's tests.
-Spins up a cog and runs locally if you're local; hits staged model in github
+Spins up a cog server and hits it with the HTTP API if you're local; runs through the python client if you're not
 """
 
 
 import base64
 import os
+import subprocess
+import sys
 import pytest
 import requests
+import replicate
+from functools import partial
 
-@pytest.fixture(scope="session")
-def model_endpoint():
-    return os.getenv('TEST_URL', "http://localhost:5000/predictions")  # Default to local URL
+ENV = os.getenv('TEST_ENV', 'local')
+LOCAL_ENDPOINT = "http://localhost:5000/predictions"
+MODEL = os.getenv('REPLICATE_MODEL', 'replicate/hello_world')
 
-@pytest.fixture(scope="session", autouse=True)
-def start_local_service_if_needed(model_endpoint):
-    if "localhost" in model_endpoint:
-        # TODO: start cog model if we're running locally.
-        yield
-    else:
-        yield
-
-def run_generation(model_endpoint, **kwargs):
-    # TODO - probably some API token shenanigans to do here.
+def local_http_generation(model_endpoint, **kwargs):
     response = requests.post(model_endpoint, json={"input": kwargs})
     data = response.json()
 
@@ -37,4 +32,39 @@ def run_generation(model_endpoint, **kwargs):
         raise e
 
 
-# TODO: define a few tests and run them, locally and otherwise.
+def replicate_run(model, version, **kwargs):
+    output = replicate.run(
+        f"{model}:{version}",
+        input=kwargs)
+    return output
+
+
+@pytest.fixture(scope="session")
+def inference_func():
+    if ENV == 'local':
+        return partial(local_http_generation, LOCAL_ENDPOINT)
+    model = replicate.models.get(MODEL)
+    version = model.versions.list()[0]
+    return partial(replicate_run, MODEL, version)
+
+
+@pytest.fixture(scope="session", autouse=True)
+def service():
+    if ENV == 'local':
+        # starts local server if we're running things locally
+        build_command = 'cog build -t test-model'.split()
+        subprocess.check_call(build_command)
+
+        run_command = 'docker run -d -p 5000:5000 --gpus all test-model'.split()
+        process = subprocess.Popen(run_command, stdout=sys.stdout, stderr=sys.stderr)
+
+        yield
+        process.terminate()
+        process.wait()
+    else:
+        yield
+
+
+
+def test_upscale(inference_func):
+    
